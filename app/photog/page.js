f8fefca085dde235511da075f7dcffe5f755f2f2
avashraj <avashraj328@gmail.com>
2024-02-26 16:26:30 -0800

    forgot to save stuff

--- app/photog/page.js
+++ app/photog/page.js
@@ -1,5 +1,6 @@
 //need home button component
-/*need photog year component 
+/*photography page:
+need photog year component 
 button for the year to show my photos for that year drop down type thing to pick different years
 favorite photos button
 all photos button
@@ -11,7 +12,7 @@ export default function Photography() {
     return(
         <div>
             <div className="flex"><button>home</button></div>
-            <div className="flex justify-center"><p className="text-4xl">One of my hobbies is taking pictures</p></div>
+            <div className="flex justify-center"><p className="text-4xl">One of my hobbies is taking pictures,</p></div>
             <div className="flex justify-center"><h1 className="text-6xl">so heres my photography</h1></div>
             <div></div>
         </div>
